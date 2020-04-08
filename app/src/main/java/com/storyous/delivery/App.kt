283b0595ca526d4189913c28b8d675c1a4ae83f6
ibartj <jan.bartovsky@storyous.com>
2020-04-08 14:13:03 +0200

    Initial commit

---
+++ app/src/main/java/com/storyous/delivery/App.kt
@@ -0,0 +1,17 @@
+package com.storyous.delivery
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
+
---
+++ app/src/main/java/com/storyous/delivery/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.storyous.delivery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("TooManyFunctions")
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

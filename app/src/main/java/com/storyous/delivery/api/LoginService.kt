938ff3d3cb381e302b266852e9e10fa0f31a6426
ibartj <jan.bartovsky@storyous.com>
2020-04-17 15:06:51 +0200

    auth param origin instead of client secret

--- app/src/main/java/com/storyous/delivery/api/LoginService.kt
+++ app/src/main/java/com/storyous/delivery/api/LoginService.kt
@@ -1,22 +1,20 @@
 package com.storyous.delivery.api
 
+import com.storyous.delivery.BuildConfig
 import okhttp3.ResponseBody
 import retrofit2.HttpException
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 import java.io.IOException
 
 interface LoginService {
 
     @FormUrlEncoded
     @POST("auth/authorize")
+    @Headers("Origin: ${BuildConfig.LOGIN_ORIGIN_HEADER}")
     @Throws(HttpException::class, IOException::class)
     suspend fun authorize(
         @Field("grant_type") grantType: String,
         @Field("client_id") clientId: String,
-        @Field("client_secret") clientSecret: String,
         @Field("code") code: String
     ): ResponseBody
 
